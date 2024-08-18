365a11d05419248c57de52484a0525fac26f174f
itsrishu02 <108127292+itsrishu02@users.noreply.github.com>
2024-08-18 12:34:10 +0530

    Add files via upload

---
+++ preprocess.py
@@ -0,0 +1,170 @@
+import pandas as pd
+import numpy as np
+import pickle as pkl
+from tqdm import tqdm
+import sys
+
+class Dataset(object):
+    def __init__(self, neg_size=99):
+        ratings = []
+        with open(sys.argv[1]+'.txt') as f:
+            for l in f:
+                user, item, _, rating, _, timestamp = [int(float(_)) for _ in l.strip().split('  ')]
+                ratings.append({
+                    'user': user,
+                    'item': item,
+                    'rating': rating,
+                    'timestamp': timestamp,
+                    })
+        ratings = pd.DataFrame(ratings)
+        item_count = ratings['item'].value_counts()
+        item_count.name = 'item_count'
+        ratings = ratings.join(item_count, on='item')
+        self.ratings = ratings
+
+        # # test set
+        # ################# if 80% training data ####################
+        # self.ratings['timerank'] = self.ratings.groupby('user')['timestamp'].rank(ascending=False).astype('int')
+        # # count = 0
+        # # for i in ratings['timerank']:
+        # #     if i==2 or i==1 or i==3 or i==4:
+        # #         count+=1
+        # # print(count)
+        # self.ratings['test_mask'] = self.ratings['timerank'].isin([1, 2, 3, 4])
+
+        # # remove items that only appear in test set
+        # items_selected = self.ratings[self.ratings['timerank'] > 4]['item'].unique()
+        # self.ratings = self.ratings[self.ratings['item'].isin(items_selected)].copy()
+        # users_selected = self.ratings[self.ratings['timerank'] > 4]['user'].unique()
+        # self.ratings = self.ratings[self.ratings['user'].isin(users_selected)].copy()
+
+        ################### if 60% training data #######################
+        self.ratings['timerank'] = self.ratings.groupby('user')['timestamp'].rank(ascending=False).astype('int')
+        self.ratings['test_mask'] = self.ratings['timerank'].isin([1, 2, 3, 4, 5, 6, 7, 8])
+
+        # remove items that only appear in test set
+        items_selected = self.ratings[self.ratings['timerank'] > 8]['item'].unique()
+        self.ratings = self.ratings[self.ratings['item'].isin(items_selected)].copy()
+        users_selected = self.ratings[self.ratings['timerank'] > 8]['user'].unique()
+        self.ratings = self.ratings[self.ratings['user'].isin(users_selected)].copy()
+
+        # drop users and movies which do not exist in ratings
+        self.users = self.ratings[['user']].drop_duplicates(subset=['user'],keep='first')
+        self.items = self.ratings[['item']].drop_duplicates(subset=['item'],keep='first')
+        self.rates = self.ratings[['rating']].drop_duplicates(subset=['rating'],keep='first')
+
+        self.users_invmap = {u: i for i, u in enumerate(self.users['user'])}
+        self.items_invmap = {m: i for i, m in enumerate(self.items['item'])}
+        self.ratings_invmap = {r: i for i, r in enumerate(self.rates['rating'])}
+
+        self.ratings['user_idx'] = self.ratings['user'].apply(lambda x: self.users_invmap[x])
+        self.ratings['item_idx'] = self.ratings['item'].apply(lambda x: self.items_invmap[x])
+        self.ratings['rating_idx'] = self.ratings['rating'].apply(lambda x: self.ratings_invmap[x])
+
+        # parse item features
+        self.num_items = len(self.items)
+        self.num_users = len(self.users)
+
+        # unobserved items for each user in training set
+        self.neg_train = [None] * len(self.users)
+
+        # negative examples for test ranking
+        self.neg_test = np.zeros((len(self.users), neg_size), dtype='int64')
+        rating_groups = self.ratings.groupby('user_idx')
+
+        for u in tqdm(range(len(self.users))):
+            interacted_items = self.ratings['item_idx'].iloc[rating_groups.indices[u]]
+            timerank = self.ratings['timerank'].iloc[rating_groups.indices[u]]
+
+            interacted_items_test = interacted_items[timerank >= 1]
+            neg_samples = np.setdiff1d(np.arange(len(self.items)), interacted_items_test)
+            self.neg_test[u] = np.random.choice(neg_samples, neg_size)
+
+            del neg_samples
+
+
+if __name__ == "__main__":
+    history_u, history_i, history_ur, history_ir = {}, {}, {}, {}
+    data = Dataset()
+    ratings = data.ratings
+    ratings_train = ratings[~(ratings['test_mask'])]
+    user_idx = ratings_train['user_idx']
+    item_idx = ratings_train['item_idx']
+    rating_idx = ratings_train['rating_idx']
+    ratings_set = set()
+
+    for u, i, r in zip(user_idx, item_idx, rating_idx):
+        ratings_set.add(r)
+        if u not in history_u:
+            history_u[u] = []
+            history_ur[u] = []
+        history_u[u].append(i)
+        history_ur[u].append(r)
+
+        if i not in history_i:
+            history_i[i] = []
+            history_ir[i] = []
+        history_i[i].append(u)
+        history_ir[i].append(r)
+
+    assert data.num_users == len(history_u)
+    assert data.num_items == len(history_i)
+
+    train_u, train_i, train_r = [], [], []
+    test_u, test_i, test_r = [], [], []
+
+    ratings_test = ratings[ratings['test_mask']]
+
+    for u, i, r in zip(ratings_train['user_idx'], ratings_train['item_idx'], ratings_train['rating']):
+        train_u.append(u)
+        train_i.append(i)
+        train_r.append(r)
+
+
+    for u, i, r in zip(ratings_test['user_idx'], ratings_test['item_idx'], ratings_test['rating']):
+        test_u.append(u)
+        test_i.append(i)
+        test_r.append(r)
+
+    social_neighbor = {}
+    num_neighbor = 0
+    with open(sys.argv[2]+'.txt', 'r', encoding='utf-8') as f:
+        for line in f:
+            user1id, user2id = line.strip().split('  ')
+            user1id, user2id = int(float(user1id)), int(float(user2id))
+            if user1id not in data.users_invmap or user2id not in data.users_invmap:
+                continue
+            user1id_idx, user2id_idx = data.users_invmap[user1id], data.users_invmap[user2id]
+            if user1id_idx not in social_neighbor:
+                social_neighbor[user1id_idx] = []
+            social_neighbor[user1id_idx].append(user2id_idx)
+            num_neighbor += 1
+
+    if len(social_neighbor) != data.num_users:
+        print('# of users have friends: '+str(len(social_neighbor)))
+        nofriends_users = np.setdiff1d(np.arange(data.num_users), list(social_neighbor.keys()))
+        for u in nofriends_users:
+            social_neighbor[u] = [u]
+            num_neighbor += 1
+
+    test_rank = {}
+
+    for u, i, r in zip(test_u, test_i, test_r):
+        if r < 3:
+            continue
+        neg_samples = data.neg_test[u]
+        test_rank[u] = {'pos': i, 'neg':neg_samples}
+        
+
+    f = open('dataset.pkl', 'wb')
+    data_content = (history_u, history_i, history_ur, history_ir, train_u, train_i, train_r,
+                     test_u, test_i, test_r, social_neighbor, list(ratings_set))
+    pkl.dump(data_content, f)
+    f.close()
+
+    print('# of users: '+str(data.num_users))
+    print('# of items: '+str(data.num_items))
+    print('# of train interactions: '+str(len(train_u)))
+    print('# of test interactions: '+str(len(test_u)))
+    print('# of rating: '+str(len(list(ratings_set))))
+    print('# of neighbors: '+str(num_neighbor))
---
+++ project.py
@@ -0,0 +1,273 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import pickle
+import numpy as np
+import torch.utils.data
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from math import sqrt
+import os
+
+class GraphRec(nn.Module):
+    def __init__(self , num_users , num_items , num_ratings , history_u , history_i , history_ur ,\
+                                     history_ir , embed_dim , social_neighbor , cuda = 'cpu'):
+        super(GraphRec, self).__init__()
+        self.embed_dim = embed_dim
+        u2e = nn.Embedding(num_users , self.embed_dim)
+        i2e = nn.Embedding(num_items , self.embed_dim)
+        r2e = nn.Embedding(num_ratings , self.embed_dim)
+        self.enc_u = ui_aggregator(i2e , r2e , u2e , embed_dim , history_u , history_ur , cuda , user=True)
+        self.enc_i = ui_aggregator(i2e , r2e , u2e , embed_dim , history_i , history_ir , cuda , user=False)
+        self.enc_social = social_aggregator(None , u2e , embed_dim , social_neighbor , cuda)
+
+        self.w_u = nn.Linear(2*self.embed_dim , self.embed_dim)
+        self.w_ur1 = nn.Linear(self.embed_dim , self.embed_dim)
+        self.w_ur2 = nn.Linear(self.embed_dim , self.embed_dim)
+        self.w_ir1 = nn.Linear(self.embed_dim , self.embed_dim)
+        self.w_ir2 = nn.Linear(self.embed_dim , self.embed_dim)
+        self.w_ui1 = nn.Linear(self.embed_dim * 2, self.embed_dim)
+        self.w_ui2 = nn.Linear(self.embed_dim , 16)
+        self.w_ui3 = nn.Linear(16 , 1)
+        self.r2e = r2e
+        self.bn1 = nn.BatchNorm1d(self.embed_dim , momentum=0.5)
+        self.bn2 = nn.BatchNorm1d(self.embed_dim , momentum=0.5)
+        self.bn3 = nn.BatchNorm1d(self.embed_dim , momentum=0.5)
+        self.bn4 = nn.BatchNorm1d(16 , momentum=0.5)
+        self.criterion = nn.MSELoss()
+
+    def forward(self , nodes_u , nodes_i):
+        item_space = self.enc_u(nodes_u)
+        social_space = self.enc_social(nodes_u)
+        user_latent_feature = torch.cat([item_space , social_space], dim=1)
+        user_latent_feature = F.relu(self.w_u(user_latent_feature))
+        item_latent_feature = self.enc_i(nodes_i)
+
+        user_latent_feature = F.relu(self.bn1(self.w_ur1(user_latent_feature)))
+        user_latent_feature = F.dropout(user_latent_feature, training=self.training)
+        user_latent_feature = self.w_ur2(user_latent_feature)
+        item_latent_feature = F.relu(self.bn2(self.w_ir1(item_latent_feature)))
+        item_latent_feature = F.dropout(item_latent_feature , training=self.training)
+        item_latent_feature = self.w_ir2(item_latent_feature)
+
+        latent_feature = torch.cat((user_latent_feature , item_latent_feature), 1)
+        latent_feature = F.relu(self.bn3(self.w_ui1(latent_feature)))
+        latent_feature = F.dropout(latent_feature , training=self.training)
+        latent_feature = F.relu(self.bn4(self.w_ui2(latent_feature)))
+        latent_feature = F.dropout(latent_feature , training=self.training)
+        scores = self.w_ui3(latent_feature)
+
+        return scores.squeeze()
+
+    def loss(self , nodes_u , nodes_i , ratings):
+        scores = self.forward(nodes_u , nodes_i)
+        return self.criterion(scores , ratings)
+
+class Attention(nn.Module):
+    def __init__(self , embedding_dims):
+        super(Attention , self).__init__()
+        self.emb_dim = embedding_dims
+        self.bilinear = nn.Bilinear(self.emb_dim, self.emb_dim, 1)
+        self.att1 = nn.Linear(self.emb_dim * 2 , self.emb_dim)
+        self.att2 = nn.Linear(self.emb_dim , self.emb_dim)
+        self.att3 = nn.Linear(self.emb_dim , 1)
+        self.softmax = nn.Softmax(0)
+
+    def forward(self , node1 , u_rep , num_neighs):
+        uv_reps = u_rep.repeat(num_neighs , 1)
+        x = torch.cat((node1 , uv_reps) , 1)
+        x = F.relu(self.att1(x))
+        x = F.dropout(x , training=self.training)
+        x = F.relu(self.att2(x))
+        x = F.dropout(x , training=self.training)
+        x = self.att3(x)
+        att = F.softmax(x , dim=0)
+        return att
+
+class ui_aggregator(nn.Module):
+    def __init__(self , i2e , r2e , u2e , embed_dim , history_ui , history_r , cuda = "cpu" , user = True):
+        super(ui_aggregator, self).__init__()
+        self.user = user
+        self.i2e = i2e
+        self.r2e = r2e
+        self.u2e = u2e
+        self.device = cuda
+        self.embed_dim = embed_dim
+        self.history_ui = history_ui
+        self.history_r = history_r
+        self.w_r1 = nn.Linear(self.embed_dim*2 , self.embed_dim)
+        self.w_r2 = nn.Linear(self.embed_dim , self.embed_dim)
+        self.att = Attention(self.embed_dim)
+        self.linear1 = nn.Linear(2*self.embed_dim , self.embed_dim) 
+
+    def forward(self, nodes):
+        ui_history = []
+        r_history = []
+        for node in nodes:
+            ui_history.append(self.history_ui[int(node)])
+            r_history.append(self.history_r[int(node)])
+
+        num_len = len(ui_history)
+        embed_matrix = torch.empty(num_len , self.embed_dim , dtype = torch.float).to(self.device)
+
+        for i in range(num_len):
+            history = ui_history[i]
+            num_histroy_ui = len(history)
+            tmp_label = r_history[i]
+
+            if self.user == True:
+                e_ui = self.i2e.weight[history]
+                ui_rep = self.u2e.weight[nodes[i]]
+            else:
+                e_ui = self.u2e.weight[history]
+                ui_rep = self.i2e.weight[nodes[i]]
+
+            e_r = self.r2e.weight[tmp_label]
+            x = torch.cat((e_ui , e_r) , 1)
+            x = F.relu(self.w_r1(x))
+            o_history = F.relu(self.w_r2(x))
+
+            att_w = self.att(o_history , ui_rep , num_histroy_ui)
+            att_history = torch.mm(o_history.t() , att_w)
+            att_history = att_history.t()
+
+            embed_matrix[i] = att_history
+        neigh_feats = embed_matrix
+
+        if self.user == True:
+            self_feats = self.u2e.weight[nodes]
+        else:
+            self_feats = self.i2e.weight[nodes]
+        combined = torch.cat([self_feats , neigh_feats] , dim = 1)
+        combined_feats = F.relu(self.linear1(combined))
+        return combined_feats
+
+class social_aggregator(nn.Module):
+  
+    def __init__(self , features , u2e , embed_dim , social_neighbor , cuda = "cpu"):
+        super(social_aggregator , self).__init__()
+        self.features = features
+        self.device = cuda
+        self.u2e = u2e
+        self.embed_dim = embed_dim
+        self.social_neighbor = social_neighbor
+        self.att = Attention(self.embed_dim)
+        self.linear1 = nn.Linear(2*self.embed_dim , self.embed_dim)
+
+    def forward(self, nodes):
+        to_neighs = []
+        for node in nodes:
+            to_neighs.append(self.social_neighbor[int(node)])
+        num_len = len(nodes)
+        embed_matrix = torch.empty(num_len , self.embed_dim , dtype = torch.float).to(self.device)
+        for i in range(num_len):
+            tmp_adj = to_neighs[i]
+            num_neighs = len(tmp_adj)
+            e_u = self.u2e.weight[list(tmp_adj)] # user embedding 
+
+            u_rep = self.u2e.weight[nodes[i]]
+
+            att_w = self.att(e_u , u_rep , num_neighs)
+            att_history = torch.mm(e_u.t() , att_w).t()
+            embed_matrix[i] = att_history
+        neigh_feats = embed_matrix
+
+        self_feats = self.u2e.weight[nodes]
+        combined = torch.cat([self_feats , neigh_feats] , dim = 1)
+        combined_feats = F.relu(self.linear1(combined))
+
+        return combined_feats
+
+
+############################################ run_graphrec ###########################################
+def train(model, device, train_loader, 
+          optimizer, epoch, best_rmse, best_mae):
+    
+    model.train()
+    running_loss = 0.0
+    for i, data in enumerate(train_loader):
+        batch_nodes_u, batch_nodes_i, batch_ratings = data
+        optimizer.zero_grad()
+        loss = model.loss(batch_nodes_u.to(device), batch_nodes_i.to(device), batch_ratings.to(device))
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+        if i % 60 == 0:
+            print('[%d, %5d] loss: %.3f, The best rmse/mae: %.6f / %.6f' % (
+                epoch, i, running_loss / 60, best_rmse, best_mae))
+            running_loss = 0.0
+
+    return 0
+
+def test(model, device, test_loader):
+    model.eval()
+    pred = []
+    target = []
+    with torch.no_grad():
+        for test_u, test_i, test_ratings in test_loader:
+            test_u, test_i, test_ratings = test_u.to(device), test_i.to(device), test_ratings.to(device)
+            scores = model(test_u, test_i)
+            pred.append(list(scores.cpu().numpy()))
+            target.append(list(test_ratings.cpu().numpy()))
+    pred = np.array(sum(pred, []))
+    target = np.array(sum(target, []))
+    rmse = sqrt(mean_squared_error(pred, target))
+    mae = mean_absolute_error(pred, target)
+
+    return rmse, mae
+
+def main():
+    os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+    use_cuda = False
+    if torch.cuda.is_available():
+        use_cuda = True
+    device = torch.device("cuda" if use_cuda else "cpu")
+    embed_dim = 128
+
+    path_data = "dataset.pkl"
+    data_file = open(path_data, 'rb')
+    history_u, history_i, history_ur, history_ir, train_u, train_i, train_r,\
+                 test_u, test_i, test_r, social_neighbor, ratings = pickle.load(data_file)
+
+    trainset = torch.utils.data.TensorDataset(torch.LongTensor(train_u), torch.LongTensor(train_i),
+                                              torch.FloatTensor(train_r))
+    
+    testset = torch.utils.data.TensorDataset(torch.LongTensor(test_u), torch.LongTensor(test_i),
+                                             torch.FloatTensor(test_r))
+
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=True)
+    num_users = history_u.__len__()
+    num_items = history_i.__len__()
+    num_ratings = ratings.__len__()
+
+    # GrapgRec Model
+    graphrec = GraphRec(num_users, num_items, num_ratings, history_u, history_i, history_ur,\
+                                     history_ir, embed_dim, social_neighbor, cuda=device).to(device)
+    optimizer = torch.optim.Adam(graphrec.parameters(), lr=0.01, betas=(0.9, 0.999))
+
+    best_rmse = 9999.0
+    best_mae = 9999.0
+    endure_count = 0
+ 
+    for epoch in range(20):
+
+        train(graphrec, device, train_loader, optimizer, epoch+1, best_rmse, best_mae)
+        expected_rmse, mae = test(graphrec, device, test_loader)
+
+        # for early stopping
+        if best_rmse > expected_rmse:
+            best_rmse = expected_rmse
+            best_mae = mae
+            endure_count = 0
+        else:
+            endure_count += 1
+        print("rmse: %.4f, mae:%.4f " % (expected_rmse, mae))
+
+        if endure_count > 5:
+            break
+    test_rmse, test_mae = test(graphrec, device, test_loader)   
+    print("test_rmse: %.4f, test_mae:%.4f " % (test_rmse, test_mae))
+
+if __name__ == "__main__":
+    main()
